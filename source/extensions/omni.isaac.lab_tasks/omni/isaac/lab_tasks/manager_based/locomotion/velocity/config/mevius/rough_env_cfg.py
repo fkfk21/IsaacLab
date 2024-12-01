253624b22e04001ec159bb239aff3c1f163253b8
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-12-01 16:44:05 +0900

    tune parameters and setup environment for mevius

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/mevius/rough_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/mevius/rough_env_cfg.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+import math
 from omni.isaac.lab.utils import configclass
 
 from omni.isaac.lab_tasks.manager_based.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg
@@ -12,6 +13,7 @@ from omni.isaac.lab_tasks.manager_based.locomotion.velocity.velocity_env_cfg imp
 ##
 from omni.isaac.lab_assets.mevius import MEVIUS_CFG  # isort: skip
 
+from omni.isaac.lab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 
 @configclass
 class MeviusRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
@@ -20,6 +22,69 @@ class MeviusRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         super().__post_init__()
         # switch robot to anymal-d
         self.scene.robot = MEVIUS_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")  # type: ignore
+        self.scene.num_envs = 1024
+
+        # scale down the terrains because the robot is small
+        self.scene.terrain.terrain_generator.sub_terrains["boxes"].grid_height_range = (0.025, 0.1)  # type: ignore
+        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].noise_range = (0.01, 0.06)  # type: ignore
+        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].noise_step = 0.01  # type: ignore
+
+        # reduce action scale
+        self.actions.joint_pos.scale = 0.5
+        self.decimation = 4
+
+        # event
+        self.events.push_robot = None  # type: ignore
+        self.events.add_base_mass.params["mass_distribution_params"] = (-1.0, 3.0)
+        self.events.add_base_mass.params["asset_cfg"].body_names = "base"
+        self.events.base_external_force_torque.params["asset_cfg"].body_names = "base"
+        self.events.reset_robot_joints.params["position_range"] = (1.0, 1.0)
+        self.events.reset_base.params = {
+            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
+            "velocity_range": {
+                "x": (0.0, 0.0),
+                "y": (0.0, 0.0),
+                "z": (0.0, 0.0),
+                "roll": (0.0, 0.0),
+                "pitch": (0.0, 0.0),
+                "yaw": (0.0, 0.0),
+            },
+        }
+
+        # commands
+        self.commands.base_velocity.heading_command = False
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.7, 0.7)
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.5, 0.5)
+        self.commands.base_velocity.ranges.ang_vel_z = (-0.7, 0.7)
+        self.commands.base_velocity.ranges.heading = (-math.pi, math.pi)
+
+        # observations
+        self.observations.policy.joint_pos.noise = Unoise(n_min=-0.05, n_max=0.05)
+        self.observations.policy.joint_vel.noise = Unoise(n_min=-1.5, n_max=1.5)
+        self.observations.policy.base_lin_vel.noise = Unoise(n_min=-0.2, n_max=0.2)
+        self.observations.policy.base_ang_vel.noise = Unoise(n_min=-0.3, n_max=0.3)
+        self.observations.policy.projected_gravity.noise = Unoise(n_min=-0.1, n_max=0.1)
+        self.observations.policy.height_scan.noise = Unoise(n_min=-0.1, n_max=0.1)
+
+        # rewards
+        self.rewards.feet_air_time.params["sensor_cfg"].body_names = ".*_foot"
+        self.rewards.undesired_contacts.params["sensor_cfg"].body_names = ["base", ".*_thigh", ".*_scapula"]
+        # reward scales
+        self.rewards.track_lin_vel_xy_exp.weight = 1.0
+        self.rewards.track_ang_vel_z_exp.weight = 0.5
+        self.rewards.action_rate_l2.weight = -0.1
+        self.rewards.flat_orientation_l2.weight = -10.0
+        self.rewards.lin_vel_z_l2.weight = -10.0
+        self.rewards.ang_vel_xy_l2.weight = -0.1
+        self.rewards.dof_torques_l2.weight = -1.0e-3
+        self.rewards.feet_air_time.weight = 0.001
+        self.rewards.dof_acc_l2.weight = -1.0e-4
+        self.rewards.dof_vel_l2.weight = -1.0e-7
+        # self.rewards.stand_still.weight = -10.0
+        self.rewards.dof_pos_limits.weight = -10.0
+
+        # terminations
+        self.terminations.base_contact.params["sensor_cfg"].body_names = ["base", ".*_thigh", ".*_scapula"]
 
 
 @configclass
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -251,6 +251,7 @@ class RewardsCfg:
     )
     # -- optional penalties
     flat_orientation_l2 = RewTerm(func=mdp.flat_orientation_l2, weight=0.0)
+    dof_vel_l2 = RewTerm(func=mdp.joint_vel_l2, weight=0.0)
     dof_pos_limits = RewTerm(func=mdp.joint_pos_limits, weight=0.0)
 
 
